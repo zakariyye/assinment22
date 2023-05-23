536e010416d84e1dab790978a9e89d58bf7873a8
zakariyye <cilmicismaan274@gmail.com>
2023-05-23 03:11:15 -0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UpdateStates from './UpdateStates';
+import DisplayList from './DisplayList';
+import Bulb from './Bulb';
+
+function App() {
+  return (
+    <div>
+      <UpdateStates />
+      <DisplayList />
+      <Bulb />
+    </div>
+  );
+}
+
+export default App;
